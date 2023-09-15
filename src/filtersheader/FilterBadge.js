2e88cb0f054565e9227eb66996960957a83368bb
FoXcodeZ <m.grabowy@int.pl>
2023-09-15 10:18:30 +0200

    frontend is connecting with backed

--- src/filtersheader/FilterBadge.js
+++ src/filtersheader/FilterBadge.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class FilderBadge extends Component {
-    constructor(props) {
-        super(props);
+class FilterBadge extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-        this.state = {
-            selected: false,
-        }
-    }
-
-    render() {
-        return (
-            <div className="inline-flex items-center rounded-md bg-green-50 px-3 py-1 text-xs
-            font-medium text-green-700 ring-1 bg-size-xs ring-inset ring-green-600/20">
-                {this.props.children}
-            </div>
-        )
-    }
+  render() {
+    const { name } = this.props;
+    
+    return (
+      <div
+        className="inline-flex items-center rounded-md bg-green-50 px-3
+        py-1 text-xs font-medium text-green-700 ring-1 bg-size-xs ring-inset ring-green-600/20"
+        >
+        {name}
+      </div>
+    );
+  }
 }
 
-export default FilderBadge;
+export default FilterBadge;
--- src/mainpage/MainPage.js
+++ src/mainpage/MainPage.js
@@ -1,8 +1,13 @@
+import FiltersNavbar from "../filtersheader/FiltersNavbar";
+import FilterBadge from "../filtersheader/FilterBadge";
 import Footer from "../footer/Footer";
 
 const MainPage = () => (
     <div>
         <p>Main Page</p>
+        <FiltersNavbar>
+            <FilterBadge></FilterBadge>
+        </FiltersNavbar>
         {/*<FetchApi />*/}
         <Footer/>
     </div>
