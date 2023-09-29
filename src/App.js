a84e7949c933d77e2bbbfe27b789c53cf9f904d3
sebgaw12 <sebastian.gawrys1@gmail.com>
2023-09-29 12:39:52 +0200

    add react-modal for display review

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Routing from "./routing/Routing";
 import {UserContextProvider} from "./context/UserContextProvider";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Modal from "react-modal";
 
 function App() {
+    Modal.setAppElement('#root')
     return (
         <div className="App bg-yellow-500">
             <UserContextProvider>
