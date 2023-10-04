974d02ea797f5ee69deac6e4d224d68b11f53800
FoXcodeZ <kitsune.dev@gmail.com>
2023-10-04 20:39:58 +0200

    FIXED filter request

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements-react/dist/js/**/*.js"
+  ],
   theme: {
     extend: {},
   },
+  darkMode: "class",
   plugins: [
-    require('@tailwindcss/forms'),
-  ],
+      require('@tailwindcss/forms'),
+      require("tw-elements-react/dist/plugin.cjs")
+  ]
 }
