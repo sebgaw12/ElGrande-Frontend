f6576047eb972a18efca661763a8ffebe4d4b2d1
sebgaw12 <sebastian.gawrys1@gmail.com>
2023-10-10 19:15:33 +0200

    removing react modal dependency

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import Routing from "./routing/Routing";
 import {UserContextProvider} from "./context/UserContextProvider";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Modal from "react-modal";
 import {RestaurantContextProvider} from "./context/RestaurantContextProvider";
 
 function App() {
-    Modal.setAppElement('#root')
+
     return (
         <div className="App bg-[rgb(255,245,0)] bg-custom-gradient">
             <RestaurantContextProvider>
