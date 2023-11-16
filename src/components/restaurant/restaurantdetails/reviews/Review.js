7297f51768481c5818837eeb4f13bca6cc94c555
sebgaw12 <sebastian.gawrys1@gmail.com>
2023-11-16 12:55:56 +0100

    styles for review

--- src/components/restaurant/restaurantdetails/reviews/Review.js
+++ src/components/restaurant/restaurantdetails/reviews/Review.js
@@ -1,11 +1,14 @@
+import {ReviewDetails} from "./Review.style";
+
 const Review = ({review}) => {
     return (
-        <div className="p-2 border-black border-2 m-2">
-            <div>{review.comment}</div>
+        <ReviewDetails>
             <div>Rating: {review.grade}</div>
-            <div>Submission date: {review.submissionTime}</div>
+            <div>{review.comment}</div>
             <div>Added by: {review.name}</div>
-        </div>
+            <div>{review.submissionTime}</div>
+
+        </ReviewDetails>
     )
 }
 
